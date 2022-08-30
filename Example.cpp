cbfa20fe8e7abda6cd6ac75c45c01d2f851a8626
Samu.py <80702161+Samu-2@users.noreply.github.com>
2022-08-30 17:15:59 -0400

    Added example WIP

--- Example.cpp
+++ Example.cpp
@@ -1,13 +1,16 @@
-#include <iostream>
-#include <vector>
-
+/* #=#=#=#=#=# INCLUDE #=#=#=#=#=# */
+#include <bits/stdc++.h>
 using namespace std;
 
-// New defines
+/* #=#=#=#=#=#  MACROS   #=#=#=#=#=# */
+#define FORN()
+#define INPUT(var, type) type var; cin >> var
+#define ININT(var) INPUT(var, int)
+#define INLL (var) INPUT(var, ll)
+#define INVEC(var) m_i arr(n)
+#define INSET(var, lenght)
+#define INMST(var, lenght)
 
-#define CIN(x, type) type x; cin >> x
-#define CINVR(x) CIN(x, auto); 
-#define CINT(x)  CIN(x, int);
 
 
 // Debug
@@ -17,14 +20,30 @@ using namespace std;
 #define cint(x) int x; cin >> x
 #define cinll(x) ll x; cin >> x
 #define sz(v) v.size()
-
 #define cinfor(x, index) cint(x); forn(x, index)
 
-auto solve(){
-    
-}
+/* #=#=#=#=#=#  TYPEDEF  #=#=#=#=#=# */
+typedef long long ll;
+typedef double    ld;
+// multiset
+typedef multiset<int> m_i;
+typedef multiset<ll>  m_ll;
+typedef multiset<ld>  m_ld;
+// vector
+typedef vector<char> v_c;
+typedef vector<int>	 v_i;
+typedef vector<ll> 	 v_ll;
+typedef vector<ld>   v_ld;
+// set
+typedef set<int> s_i;
+typedef set<ll>  s_ll;
+typedef set<ld>  s_ld;
 
+typedef pair<int,int> ii;
+
+/* #=#=#=#=#=#    MAIN   #=#=#=#=#=# */
 int main() {
-    solve();
+	ios::sync_with_stdio(0); cin.tie(0);
+	vector<int> arr(n); for (int &x: arr) cin >> x;
     return 0;
 }
